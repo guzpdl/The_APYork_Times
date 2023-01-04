3af141c208967e829cb82b3cef72da7feec38899
Guzman <guzpdl@gmail.com>
2023-01-04 18:08:04 +0100

    news page

--- src/components/Articles/NewsArticles/NewsArticle.jsx
+++ src/components/Articles/NewsArticles/NewsArticle.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Card, ListGroup } from "react-bootstrap";
 import TopStoriesAxios from "../../../services/topStories.service";
 
 const NewsArticle = () => {
@@ -23,8 +23,23 @@ const NewsArticle = () => {
 
   return newStories.map((newsData, index) => {
     return (
-      <Col lg={4}>
-        <h1>New Story</h1>
+      <Col lg={4} className="d-flex justify-content-around">
+        <Card className="m-2" style={{ width: "28rem" }}>
+          <Card.Img variant="top" src={newsData.multimedia[1].url} />
+          <Card.Body>
+            <Card.Title>{newsData.title}</Card.Title>
+            <Card.Text>{newsData.abstract}</Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
       </Col>
     );
   });
